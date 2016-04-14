3e658238a8307732747f0354068a16fbab299f74
radikalLol <radikal.xxxxx@gmail.com>
2016-04-14 18:11:13 +0300

    Added files via upload

---
+++ temp/ffdgjghk.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); 
+int c=0;
+uint8_t CK_A = 0, CK_B = 0;
+void setup() {
+Serial.begin(57600);
+mySerial.begin(9600);
+Serial.read();
+}
+void ubx_checksum(byte ubx_data)
+{
+  CK_A+=ubx_data;
+  CK_B+=CK_A;
+}
+void loop()
+{
+  mySerial.write(Serial);
+}
+
+
